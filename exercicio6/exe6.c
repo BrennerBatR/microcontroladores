daeeebe9f43847b34bde85364f53c70fa7936fa2
BrennerBatR <brenner.batista@outlook.com>
2020-02-20 16:29:30 -0300

    exercicio6 trisc

--- exercicio6/exe6.c
+++ exercicio6/exe6.c
@@ -4,9 +4,9 @@
 int main(){
 	//para usar pwm precisa configurar PR2,T2CON,CCPRxL, CCPxCON
 	T2CON = 0b00000001;
-	PR2 = 124;
-	CCPR1L = 300>>2;
-	CCP1CON = 0b00001100;
-	
+	PR2 = 124;			
+	CCPR1L = 300>>2; 	//8 bits mais significativos
+	CCP1CON = 0b00001100;	//2 bits menos significativos do 300 em <5:4>
+	TRISC &= ~(1<<2); //setando como output RC2
 	while(1){};
 }
